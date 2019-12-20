f8aa198b4709ff7b8410d53dbe057ccbbffb10d5
robertoSK19 <59093785+robertoSK19@users.noreply.github.com>
2019-12-20 10:18:45 -0600

    Version 0.0.0

---
+++ src/test/java/com/ska/SkaV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
